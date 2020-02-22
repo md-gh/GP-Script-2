496dd50a1ac616115d466aa344f56cb3ca3529a3
antonellocaroli <antonellocaroli@gmail.com>
2020-02-22 07:14:43 +0100

    up

--- gentooplayer/web/scripts/script/alsa/alsasw.sh
+++ gentooplayer/web/scripts/script/alsa/alsasw.sh
@@ -2,4 +2,4 @@
 
 
 #ls -1v /usr/portage/packages/media-libs/ | sed 's/.tbz2//' | sed 's/alsa-lib-//' | cat -n
-ls -1v /usr/portage/packages/media-libs/ | sed 's/.tbz2//' | sed 's/alsa-lib-//'
+ls -1v /var/cache/binpkgs/media-libs/ | sed 's/.tbz2//' | sed 's/alsa-lib-//'
