9f552a9e55fe3d37c80b948f774ca2dca66dfbeb
antonellocaroli <antonellocaroli@gmail.com>
2020-02-25 19:36:16 +0100

    up

--- gentooplayer/script/news/news.sh
+++ gentooplayer/script/news/news.sh
@@ -4,3 +4,19 @@ gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-ve
 gpmodel=$(sed -n 16p /opt/.gentooplayer/GentooPlayer/gentooplayer/.hw_model)
 
 echo -e "$BRed GentooPlayer $Color_Off $BBlue $gpmodel $Color_Off $BBlack version=$gpversion $Color_Off"
+echo
+echo -e "$BBlack Latest News:$Color_Off"
+echo
+
+echo -e "$BBlack 25.02.19 A new version of MyMpd is available v.6.2.0$Color_Off"
+echo
+echo -e "Changelog: https://github.com/jcorporation/myMPD/releases/tag/v6.2.0"
+echo
+echo "to update:
+$BBlack gp-update$Color_Off
+and
+ssh: command $BBlack mympd-up$Color_Off
+or
+web-interface>System H. Software Upd/Rem>Select Software Update>MyMpd>EXCUTE
+or
+web-interface>System A. Excute Command>mympd-up"
