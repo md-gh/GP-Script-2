9595a160786f7c2437d82526a70f70ef12740910
antonellocaroli <antonellocaroli@gmail.com>
2020-02-25 19:51:15 +0100

    up

--- gentooplayer/script/news/news.sh
+++ gentooplayer/script/news/news.sh
@@ -14,9 +14,9 @@ echo -e "Changelog: https://github.com/jcorporation/myMPD/releases/tag/v6.2.0"
 echo
 echo -e "to update:
 $BBlack gp-update$Color_Off
-and
-ssh: command $BBlack mympd-up$Color_Off (faster)
-or
-web-interface>System H. Software Upd/Rem>Select Software Update>MyMpd>EXCUTE
-or
-web-interface>System A. Excute Command>mympd-up (faster)"
+ and
+ ssh: command $BBlack mympd-up$Color_Off (faster)
+ or
+ web-interface>System H. Software Upd/Rem>Select Software Update>MyMpd>EXCUTE
+ or
+ web-interface>System A. Excute Command>mympd-up (faster)"
