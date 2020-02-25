3c699460d3d31b8c1107d55cdc8e0da350dd650c
antonellocaroli <antonellocaroli@gmail.com>
2020-02-25 19:13:44 +0100

    up

---
+++ gentooplayer/script/news/news.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+. /opt/.gentooplayer/function/felenco.sh
+gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
+gpmodel=$(sed -n 16p /opt/.gentooplayer/GentooPlayer/gentooplayer/.hw_model)
+
+echo -e "$BBlack GentooPlayer $gpmodel version=$gpversion $Color_Off"
