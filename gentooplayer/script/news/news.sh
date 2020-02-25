bdfd167df3b77438a94ebc90261a01cd20cd53ca
antonellocaroli <antonellocaroli@gmail.com>
2020-02-25 19:23:19 +0100

    up

--- gentooplayer/script/news/news.sh
+++ gentooplayer/script/news/news.sh
@@ -3,4 +3,4 @@
 gpversion="$(sed -n 1p /etc/default/.GP-version).$(sed -n 2p /etc/default/.GP-version)$(sed -n 3p /etc/default/.GP-version)"
 gpmodel=$(sed -n 16p /opt/.gentooplayer/GentooPlayer/gentooplayer/.hw_model)
 
-echo -e "$Red GentooPlayer $Color_Off $BBlue $gpmodel $BBlack $Color_Off version=$gpversion $Color_Off"
+echo -e "$Red GentooPlayer $Color_Off $BBlue $gpmodel $Color_Off $BBlack version=$gpversion $Color_Off"
