7bcb0392727dcf9363f9eb2d0d70b011d0063ad0
antonellocaroli <antonellocaroli@gmail.com>
2020-02-26 15:16:36 +0100

    Update alsasw.sh

--- gentooplayer/web/scripts/alsasw.sh
+++ gentooplayer/web/scripts/alsasw.sh
@@ -30,29 +30,43 @@ case $alsav in
     1.0.29)
         emerge --ask -k =media-libs/alsa-lib-1.0.29 =media-sound/alsa-utils-1.0.29
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.1.2)
         emerge --ask -k =media-libs/alsa-lib-1.1.2 =media-sound/alsa-utils-1.1.2
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.1.6)
         emerge --ask -k =media-libs/alsa-lib-1.1.6 =media-sound/alsa-utils-1.1.6
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.1.8)
         emerge --ask -k =media-libs/alsa-lib-1.1.8 =media-sound/alsa-utils-1.1.8
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.1.9)
         emerge --ask -k =media-libs/alsa-lib-1.1.9 =media-sound/alsa-utils-1.1.9
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.2.1.2)
         emerge --ask -k =media-libs/alsa-lib-1.2.1.2 =media-sound/alsa-utils-1.2.1
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
     1.2.2)
         emerge --ask -k =media-libs/alsa-lib-1.2.2 =media-sound/alsa-utils-1.2.2
         /etc/init.d/alsasound restart
+        echo
+        echo
         echo -e "alsa switching ok" ;;
 esac
