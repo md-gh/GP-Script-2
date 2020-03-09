80d1b30cfea0d33c78f4913eefa90d6ce9e37261
antonellocaroli <antonellocaroli@gmail.com>
2020-03-09 06:04:28 +0100

    up

--- gentooplayer/web/scripts/mpd_bp.sh
+++ gentooplayer/web/scripts/mpd_bp.sh
@@ -93,7 +93,7 @@ if [ "$ptime" = "enable" ]; then
     ptimem='period_time ''"'"$ptimev"'"'
 fi
 if [ "$dsd" = "DOP" ]; then
-    dsdd='dsd' 'yes'
+    dsdd='dsd yes'
 fi
 
 cat > /etc/mpd.conf <<EOF
@@ -126,6 +126,7 @@ audio_output {
   use_mmap  "$mmap"
   $btimem
   $ptimem
+  $dsdd
 }
 
 mixer_type   "$mixtype"
