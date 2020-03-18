ab0d748af0c4f409fd3ab287214f95e7b8ee8c8a
antonellocaroli <antonellocaroli@gmail.com>
2020-03-18 18:25:08 +0100

    up

--- gentooplayer/script/news/news.sh
+++ gentooplayer/script/news/news.sh
@@ -5,7 +5,7 @@ cd /tmp
 
 rm -r GP-news-master 2>/dev/null
 #git clone https://github.com/antonellocaroli/GP-news.git /tmp/news 2>/dev/null
-wget https://github.com/antonellocaroli/GP-news/archive/master.zip 2>/dev/null
+wget --no-check-certificate https://github.com/antonellocaroli/GP-news/archive/master.zip 2>/dev/null
 
 unzip master.zip
 
