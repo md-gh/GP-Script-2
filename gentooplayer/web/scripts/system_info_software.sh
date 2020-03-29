e4d83a63d4dadd26a9039a98e4c841ad44534ee8
antonellocaroli <antonellocaroli@gmail.com>
2020-03-29 08:05:29 +0200

    Update system_info_software.sh

--- gentooplayer/web/scripts/system_info_software.sh
+++ gentooplayer/web/scripts/system_info_software.sh
@@ -30,5 +30,45 @@ soft=$(emerge --search "%hqplayerd-bin$" 2>/dev/null | grep 'media-sound/hqplaye
 echo "$soft"
 echo
 echo -e "$BRed Mpd$color_off"
-soft=$(emerge --search "%media-sound/mpd$" 2>/dev/null | grep 'media-sound/mpd\|Latest version available:\|Latest version installed:')
+soft=$(emerge --search "%media-sound/mpd$" 2>/dev/null | grep 'media-sound/mpd\|Latest version available:\|Latest version installed:' | sed '1d')
+echo "$soft"
+echo
+echo -e "$BRed UpMpdCli$color_off"
+soft=$(emerge --search "%upmpdcli$" 2>/dev/null | grep 'media-sound/upmpdcli\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed RoonBridge$color_off"
+soft=$(emerge --search "%RoonBridge$" 2>/dev/null | grep 'media-sound/RoonBridge\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed RoonServer$color_off"
+soft=$(emerge --search "%RoonServer$" 2>/dev/null | grep 'media-sound/RoonServer\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed RoonServer$color_off"
+soft=$(emerge --search "%RoonServer$" 2>/dev/null | grep 'media-sound/RoonServer\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed MyMpd$color_off"
+soft=$(emerge --search "%mympd$" 2>/dev/null | grep 'media-sound/mympd\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed Shairport-Sync$color_off"
+soft=$(emerge --search "%shairport-sync$" 2>/dev/null | grep 'media-sound/shairport-sync\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed Hqplayer-Desktop3$color_off"
+soft=$(emerge --search "%hqplayer-bin$" 2>/dev/null | grep 'media-sound/hqplayer-bin\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed Hqplayer-Desktop4$color_off"
+soft=$(emerge --search "%hqplayer4desktop-bin$" 2>/dev/null | grep 'media-sound/hqplayer4desktop-bin\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed Jriver$color_off"
+soft=$(emerge --search "%jriver-bin$" 2>/dev/null | grep 'media-sound/jriver-bin\|Latest version available:\|Latest version installed:')
+echo "$soft"
+echo
+echo -e "$BRed Qobuz-Desktop$color_off"
+soft=$(emerge --search "%qobuz-desktop-player-bin$" 2>/dev/null | grep 'media-sound/qobuz-desktop-player-bin\|Latest version available:\|Latest version installed:')
 echo "$soft"
