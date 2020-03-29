8bb8db7b1314465041f5f14253f8a852e0d882a0
antonellocaroli <antonellocaroli@gmail.com>
2020-03-29 07:20:57 +0200

    Update system_info_software.sh

--- gentooplayer/web/scripts/system_info_software.sh
+++ gentooplayer/web/scripts/system_info_software.sh
@@ -4,6 +4,7 @@ echo
 
 . /opt/.gentooplayer/function/fcolors.sh
 
-lms=$(emerge --search "%logitechmediaserver-bin$" &>/dev/null | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:')
+echo -e "$BRed LogitechMediaServer$color_off"
+lms=$(emerge --search "%logitechmediaserver-bin$" 2>/dev/null | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:')
 #emerge --search "%logitechmediaserver-bin$" | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:'
 echo "$lms"
