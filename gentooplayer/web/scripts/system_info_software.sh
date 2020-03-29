86a3973aff9b4e2d9e2e77762bef0d3efe2d9383
antonellocaroli <antonellocaroli@gmail.com>
2020-03-29 07:38:04 +0200

    Update system_info_software.sh

--- gentooplayer/web/scripts/system_info_software.sh
+++ gentooplayer/web/scripts/system_info_software.sh
@@ -5,18 +5,18 @@ echo
 /opt/.gentooplayer/script/gp-update0.sh &>/dev/null
 
 echo -e "$BRed LogitechMediaServer$color_off"
-lms=$(emerge --search "%logitechmediaserver-bin$" 2>/dev/null | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:')
+soft=$(emerge --search "%logitechmediaserver-bin$" 2>/dev/null | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:')
 #emerge --search "%logitechmediaserver-bin$" | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:'
 echo "$lms"
 echo
 echo -e "$BRed Squeezelite$color_off"
-sqeeze=$(emerge --search "%squeezelite$" 2>/dev/null | grep 'media-sound/squeezelite\|Latest version available:\|Latest version installed:')
-echo "$sqeeze"
+soft=$(emerge --search "%squeezelite$" 2>/dev/null | grep 'media-sound/squeezelite\|Latest version available:\|Latest version installed:')
+echo "$soft"
 echo
 echo -e "$BRed Squeezelite-R2$color_off"
-sqeeze2=$(emerge --search "%squeezelite-R2$" 2>/dev/null | grep 'media-sound/squeezelite-R2\|Latest version available:\|Latest version installed:')
-echo "$sqeeze2"
+soft=$(emerge --search "%squeezelite-R2$" 2>/dev/null | grep 'media-sound/squeezelite-R2\|Latest version available:\|Latest version installed:')
+echo "$soft"
 echo
 echo -e "$BRed Networkaudiod$color_off"
-nad=$(emerge --search "%networkaudiod-bin$" 2>/dev/null | grep 'media-sound/networkaudiod-bin\|Latest version available:\|Latest version installed:')
-echo "$nad"
+soft=$(emerge --search "%networkaudiod-bin$" 2>/dev/null | grep 'media-sound/networkaudiod-bin\|Latest version available:\|Latest version installed:')
+echo "$soft"
