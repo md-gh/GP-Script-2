1bdeb659bdb5d5cd9f22946adb3b9d58724776b4
antonellocaroli <antonellocaroli@gmail.com>
2020-03-29 07:24:41 +0200

    Update system_info_software.sh

--- gentooplayer/web/scripts/system_info_software.sh
+++ gentooplayer/web/scripts/system_info_software.sh
@@ -8,3 +8,8 @@ echo -e "$BRed LogitechMediaServer$color_off"
 lms=$(emerge --search "%logitechmediaserver-bin$" 2>/dev/null | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:')
 #emerge --search "%logitechmediaserver-bin$" | grep 'media-sound/logitechmediaserver-bin\|Latest version available:\|Latest version installed:'
 echo "$lms"
+echo
+echo
+echo -e "$BRed Squeezelite$color_off"
+sqeeze=$(emerge --search "%squeezelite$" 2>/dev/null | grep 'media-sound/squeezelite\|Latest version available:\|Latest version installed:')
+echo "$sqeeze"
