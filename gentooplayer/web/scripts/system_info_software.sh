c70f69a105fcaa9e24c2ea028120c282bfc8e0ac
antonellocaroli <antonellocaroli@gmail.com>
2020-03-29 07:44:59 +0200

    Update system_info_software.sh

--- gentooplayer/web/scripts/system_info_software.sh
+++ gentooplayer/web/scripts/system_info_software.sh
@@ -28,3 +28,7 @@ echo
 echo -e "$BRed HQPlayer Embedded$color_off"
 soft=$(emerge --search "%hqplayerd-bin$" 2>/dev/null | grep 'media-sound/hqplayerd-bin\|Latest version available:\|Latest version installed:')
 echo "$soft"
+echo
+echo -e "$BRed Mpd$color_off"
+soft=$(emerge --search "%media-sound/mpd$" 2>/dev/null | grep 'media-sound/media-sound/mpd\|Latest version available:\|Latest version installed:')
+echo "$soft"
