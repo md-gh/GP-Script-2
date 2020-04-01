53687b84a0e9d85492c11dcfb09f398cbd89411b
antonellocaroli <antonellocaroli@gmail.com>
2020-04-01 03:54:57 +0200

    Create alsasw0.sh

---
+++ gentooplayer/web/scripts/alsasw0.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+repeats=1
+
+clear=false
+
+while (( $# >= 1 ))
+do
+    key="$1"
+
+    case $key in
+        -c)
+            prova="$2"
+            shift # past argument
+            ;;
+        -d)
+            prova1="$2"
+            shift # past argument
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
+echo "$prova"
+echo "$prova1"
